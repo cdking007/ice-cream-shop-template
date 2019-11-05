c2ff7d27fe1da29ef1df9140acd69df2a3902cbe
cdking007 <39514257+cdking007@users.noreply.github.com>
2019-11-05 12:59:22 +0530

    Add files via upload
    script update

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const navBtn = document.getElementById('nav-btn');
 const navbar = document.getElementById('navbar');
 const navclose = document.getElementById('nav-close');
+const navlinks = document.querySelectorAll('.nav-links');
 
 navBtn.addEventListener('click',()=>{
     navbar.classList.add('showNav');
 });
 navclose.addEventListener('click',()=>{
     navbar.classList.remove('showNav');
-})
+});
+console.log(navlinks);
+for(let i=0;i<navlinks.length;i++){
+    navlinks[i].addEventListener('click',()=>{
+        navbar.classList.remove('showNav');
+    });
+}
